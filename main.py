c30b14d7b6167027ff2d1965bcf8218eb579c758
Remy <wangrui777111@gmail.com>
2023-10-12 12:13:37 +0800

    done for 5.1

---
+++ main.py
@@ -0,0 +1,20 @@
+# Exercises Module 5
+
+import random
+
+def rolldices():
+
+    num_dices = int(input("How many dices do you want to run? "))
+
+    total = 0
+
+    for dice in range(num_dices):
+
+        roll = random.randint(1, 6)
+        total += roll
+
+    # Prints out the sum of the numbers
+    print(f"Total numbers of the dice rolls are {total} ")
+
+
+rolldices()
