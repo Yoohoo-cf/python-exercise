e1c77f3d6c61db2af7337fbf8170e529ac056466
Remy <wangrui777111@gmail.com>
2023-10-12 17:13:21 +0800

    done for 6.2

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 # Exercises Module 6.1
 
 """
-Write a function that returns a random dice roll between 1 and 6.
-The function should not have any parameters.
-Write a main program that rolls the dice until the result is 6.
-The main program should print out the result of each roll.
+Gets the number of sides on the dice as a parameter.
+With the modified function you can for example roll a 21-sided role-playing dice.
+The difference to the last exercise is that the dice rolling in
+the main program continues until the program gets the maximum
+number on the dice, which is asked from the user at the beginning.
 """
 
 import random
 
-def roll_dice():
-    return random.randint(1, 6)
+def roll_dice(sides):
+    return random.randint(1, sides)
 
 def main():
+    sides = int(input("How many sides are you playing on this dice? "))
     rolls = 0
+
     while True:
-        result = roll_dice()
+        result = roll_dice(sides)
         rolls += 1
         print(f"Roll {rolls}: {result}")
 
-        if result == 6:
+        if result == sides:
             break
 
 
