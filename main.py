f3a2951d0127919f3d63e72f70809aae9ce818c6
Remy <wangrui777111@gmail.com>
2023-10-22 21:14:01 +0800

    done with 5.4

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-# Exercises Module 6.2
 
-"""
-Gets the number of sides on the dice as a parameter.
-With the modified function you can for example roll a 21-sided role-playing dice.
-The difference to the last exercise is that the dice rolling in
-the main program continues until the program gets the maximum
-number on the dice, which is asked from the user at the beginning.
-"""
+# Exercises Module 5.4
 
-import random
+cities = []
 
-def roll_dice(sides):
-    return random.randint(1, sides)
+for i in range(5):
+    city = input("Enter a city name: ")
+    cities.append(city)
 
-def main():
-    sides = int(input("How many sides are you playing on this dice? "))
-    rolls = 0
-
-    while True:
-        result = roll_dice(sides)
-        rolls += 1
-        print(f"Roll {rolls}: {result}")
-
-        if result == sides:
-            break
-
-main()
-
+print("List of cities:")
+for city in cities:
+    print (city)
 
 
 
