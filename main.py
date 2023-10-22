e38ca7a278061b77b3323df9575bbce3a969b3d2
Yoohoo-cf <114879220+Yoohoo-cf@users.noreply.github.com>
2023-10-22 10:18:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 # Exercises Module 6.4
 
-"""
-Write a function that gets a list of integers as a parameter.
-The function returns the sum of all the numbers in the list.
-For testing, write a main program where you create a list,
-call the function, and print out the value it returned.
-"""
-
 def int_sum(list):
 
     total = sum(list)
