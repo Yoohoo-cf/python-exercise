7df68e56c92a0f2cfaa4f58afee287f973078aad
Yoohoo-cf <114879220+Yoohoo-cf@users.noreply.github.com>
2023-10-22 10:20:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-# Exercises Module 8.3
-
-"""
-Write a program that asks the user to enter the ICAO codes of two airports.
-The program prints out the distance between the two airports in kilometers.
-The calculation is based on the airport coordinates fetched from the database.
-Calculate the distance using the geopy library: https://geopy.readthedocs.io/en/stable/.
-Install the library by selecting View / Tool Windows / Python Packages in your PyCharm IDE,
-write geopy into the search field and finish the installation.
-"""
+# Exercises 8.3
 
 import mysql.connector
 from geopy import distance
