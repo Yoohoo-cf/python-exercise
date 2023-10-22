83c8551bcb48e6253135e87630c544e5eadad785
Yoohoo-cf <114879220+Yoohoo-cf@users.noreply.github.com>
2023-10-22 10:19:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 # Exercises Module 8.2
 
-"""
-Write a program that asks the user to enter the area code (for example FI)
-and prints out the airports located in that country ordered by airport type.
-For example, Finland has 65 small airports, 15 helicopter airports and so on.
-"""
-
 import mysql.connector
 
 connection = mysql.connector.connect(
