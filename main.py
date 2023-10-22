fd30994a4c837966a10237c3a22ee230781565d6
Yoohoo-cf <114879220+Yoohoo-cf@users.noreply.github.com>
2023-10-22 10:17:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 # Exercises Module 6.2
 
-"""
-Gets the number of sides on the dice as a parameter.
-With the modified function you can for example roll a 21-sided role-playing dice.
-The difference to the last exercise is that the dice rolling in
-the main program continues until the program gets the maximum
-number on the dice, which is asked from the user at the beginning.
-"""
-
 import random
 
 def roll_dice(sides):
