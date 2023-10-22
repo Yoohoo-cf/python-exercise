e357be97f31421b1059f83dab9621b69407a7158
Yoohoo-cf <114879220+Yoohoo-cf@users.noreply.github.com>
2023-10-22 10:19:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 # Exercises Module 6.5
 
-"""
-Write a function that gets a list of integers as a parameter.
-The function returns a second list that is otherwise the same as the original list
-except that all uneven numbers have been removed. For testing, write a main program
-where you create a list, call the function, and then print out both the original
-as well as the cut-down list.
-"""
-
 def remove_odd_list(list):
 
     even_list = []
