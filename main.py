8dc60223080d23e400cf82d4277748bbc6be636e
Yoohoo-cf <114879220+Yoohoo-cf@users.noreply.github.com>
2023-10-22 10:18:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 # Exercises Module 6.3
 
-"""
-Write a function that gets the quantity of gasoline in American gallons
-and returns the number converted to litres. Write a main program that
-asks for a volume in gallons from the user and converts the value to liters.
-The conversion must be done by using the function. Conversions continue
-until the user inputs a negative value.
-"""
-
 def gallons_to_litres(gallons):
 
     litres = gallons * 3.78541
